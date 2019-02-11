686636399fea1d199d2a624f9165c823f7272efe
Sagar Das <sagar.uiet8@gmail.com>
2019-02-11 20:28:46 +0530

    Combination with repitition

---
+++ Combination_with_repitition.py
@@ -0,0 +1,38 @@
+"""
+Created on Tue Feb  11 18:52:27 2019
+
+@author: sagar
+"""
+import re  # to enable splitting for any type spaces
+
+
+# Function combination takes in input sequence and the size of combination
+def combination(input_sequence, combination_size):
+    length_of_input = len(input_sequence)
+    temp_sequence = [0] * combination_size
+    divideconquer(input_sequence, temp_sequence, 0, length_of_input - 1, 0, combination_size)
+
+
+# divide nd conquer function takes in 5 arguments and is called recursively to generate
+# tree like structure for each iterable in the input sequence
+
+def divideconquer(input_sequence, temp_sequence, start, end, index, combination_size):
+    if (index == combination_size):
+        for j in range(combination_size):
+            print(temp_sequence[j])
+        print()
+        return
+    else:
+        i = start
+        while (i <= end):
+            temp_sequence[index] = input_sequence[i]
+            # Divide and conquer starts from the current node irrespective of whether it has been selected or not
+            # It doesn't iterate fro i+1
+            divideconquer(input_sequence, temp_sequence, i, end, index + 1, combination_size)
+            i += 1
+
+
+if __name__ == '__main__':
+    input_sequence = re.split(r'[;,\s]\s*', input("enter a list "))
+    combination_size = int(input("enter the size of combination "))
+    combination(input_sequence, combination_size)
