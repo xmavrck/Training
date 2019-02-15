45e63a2123a568e9355dfd9a4ed77d7728f0b3ae
ankitkumargehu <ankitgraphicera@gmail.com>
2019-02-15 19:56:00 +0530

    QuickSort

---
+++ quick/quickS.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	var n int
+	fmt.Printf("Enter the size of the array: ")
+	fmt.Scanf("%d", &n)
+	array := make([]int, n)
+	for i := 0; i < n; i++ {
+		array[i] = rand.Intn(100)
+	}
+	fmt.Println("\n--- Unsorted ---\n", array)
+	quicksort(array)
+	fmt.Println("--- Sorted ---\n", array)
+}
+
+func quicksort(array []int) []int {
+	if len(array) < 2 {
+		return array
+	}
+
+	left, right := 0, len(array)-1
+
+	pivot := rand.Int() % len(array)
+
+	array[pivot], array[right] = array[right], array[pivot]
+
+	for i := range array {
+		if array[i] < array[right] {
+			array[left], array[i] = array[i], array[left]
+			left++
+		}
+	}
+
+	array[left], array[right] = array[right], array[left]
+
+	quicksort(array[:left])
+	quicksort(array[left+1:])
+
+	return array
+}
