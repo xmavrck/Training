9e5e94ce9309505e1d89904f6c8e59bb43092d07
ankitkumargehu <ankitgraphicera@gmail.com>
2019-02-16 16:23:07 +0530

    FisherYates

---
+++ Fisher/FisherYates.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func fisheryate(array []int, n int) []int {
+	var temp int
+	var random int
+	for i := n - 1; i > 0; i-- {
+		random = rand.Intn(n - 1)
+		temp = array[i]
+		array[i] = array[random]
+		array[random] = temp
+	}
+	return array
+}
+func main() {
+	var n int
+	var array []int
+	fmt.Printf("Enter the size of the array: ")
+	fmt.Scanf("%d", &n)
+	array = make([]int, n)
+	fmt.Printf("Enter the element in the array:\n")
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%d ", &array[i])
+	}
+	res := fisheryate(array, n)
+	fmt.Println(res)
+}
