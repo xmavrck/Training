f566973938bf03861d4eec8fe52db339c8c74476
ankitkumargehu <ankitgraphicera@gmail.com>
2019-02-17 13:02:03 +0530

    DFS

---
+++ DFS/DFS.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+var graph [50][50]int
+var visited [20]int
+var n int
+
+func dFS(i int) {
+	fmt.Printf("%d ", i)
+	visited[i] = 1
+	for j := 0; j < n; j++ {
+		if visited[j] == 0 && graph[i][j] == 1 {
+			dFS(j)
+		}
+	}
+}
+func main() {
+	fmt.Printf("Enter the no of vertices: ")
+	fmt.Scanf("%d", &n)
+	fmt.Printf("Enter the adjecency matrix:\n ")
+	for i := 0; i < n; i++ {
+		fmt.Printf("enter the element in vertix: %d\n", i+1)
+		for j := 0; j < n; j++ {
+			fmt.Scanf("%d", &graph[i][j])
+		}
+	}
+	for i := 0; i < n; i++ {
+		visited[i] = 0
+	}
+	fmt.Printf("the path is: ")
+	dFS(0)
+}
