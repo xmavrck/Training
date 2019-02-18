5e9eb2fdd694abee7ed535a9fa027026275cd151
AvinashiSharma <sharma.avinashi86@gmail.com>
2019-02-18 11:43:23 -0800

    Rain Terrace

---
+++ rainTerraces.js
@@ -0,0 +1,37 @@
+function rainTerraces(terraces) {
+  let waterAmount = 0;
+
+  for (let terraceIndex = 0; terraceIndex < terraces.length; terraceIndex += 1) {
+    // Get left most high terrace.
+    let leftHighestLevel = 0;
+    for (let leftIndex = terraceIndex - 1; leftIndex >= 0; leftIndex -= 1) {
+      leftHighestLevel = Math.max(leftHighestLevel, terraces[leftIndex]);
+    }
+
+    // Get right most high terrace.
+    let rightHighestLevel = 0;
+    for (let rightIndex = terraceIndex + 1; rightIndex < terraces.length; rightIndex += 1) {
+      rightHighestLevel = Math.max(rightHighestLevel, terraces[rightIndex]);
+    }
+
+    // Add current terrace water amount.
+    const terraceBoundaryLevel = Math.min(leftHighestLevel, rightHighestLevel);
+    if (terraceBoundaryLevel > terraces[terraceIndex]) {
+      // Terrace will be able to store the water if the lowest of two left and right highest
+      // terraces are still higher than the current one.
+      waterAmount += Math.min(leftHighestLevel, rightHighestLevel) - terraces[terraceIndex];
+    }
+  }
+
+  return waterAmount;
+}
+
+//test
+let size=prompt("enter no of bars");
+let inp=[];
+for(let i=0;i<size;i++)
+  {
+   inp[i]=prompt("enter the bars to store water"); 
+  }
+
+console.log("output = "+rainTerraces(inp));
