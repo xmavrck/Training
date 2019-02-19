557b0aa38a72c3cb9cc0b69edc30f3390fb4ffdb
diveshasija133 <divesh.asija133@gmail.com>
2019-02-19 12:11:52 +0530

    Maximun_subarray

---
+++ maximunsubarray/maxsubarray.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func subArray(a []int, size int) int {
+	var max, min = -99999, 0
+
+	for i := 0 ; i < size; i++ {
+		min = min + a[i]
+		if max < min {
+			max = min
+		}
+		if min < 0 {
+			min = 0
+		}
+
+	}
+	return max
+}
+
+func main() {
+	var n int
+	var array []int
+	fmt.Printf("Enter the size of the array: ")
+	fmt.Scanf("%d ", &n)
+	array = make([]int, n)
+	fmt.Printf("Enter the sorted element in the array:\n")
+	for i := 0 ; i < n; i++ {
+		fmt.Scanf("%d ", &array[i])
+	}
+	fmt.Println("Maximum contiguous sum is ", subArray(array, n))
+}
